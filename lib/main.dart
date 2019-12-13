b1aa1c06aecccb9800f096362a0a223cdfb60879
Breyanna Vass <breyannavass@gmail.com>
2019-12-12 21:48:01 -0500

    settingup the first half and the custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,69 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/bloc/cartlistBloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Food Delivery",
-      theme: ThemeData(primaryColor: Colors.deepOrange[600]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Food Delivery'),
+    return BlocProvider(
+      blocs: [Bloc((i) => CartListBloc())],
+      child: MaterialApp(
+        title: "Food Delivery",
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: ListView(
+            children: <Widget>[
+              FirstHalf();
+            ],
+          ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
+    )
+  }
+}
+
+class FirstHalf extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.menu),
+          Container(
+            margin: EdgeInsets.only(right: 30),
+            child: Text("0"),
+          )
+        ],
+      ),
     );
   }
 }
