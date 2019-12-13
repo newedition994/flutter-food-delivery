9a8f17e98ab21d003b6132600a630cc4668d49d8
Breyanna Vass <breyannavass@gmail.com>
2019-12-12 21:28:46 -0500

    making the provider work with the cartlist

---
+++ lib/bloc/provider.dart
@@ -0,0 +1,15 @@
+import 'package:food_delivery/model/fooditem.dart';
+
+class CartProvider {
+  List<FoodItem> foodItems = [];
+
+  List<FoodItem> addToList(FoodItem foodItem) {
+    foodItems.add(foodItem);
+    return foodItems;
+  }
+
+  List<FoodItem> removeFromList(FoodItem foodItem) {
+    foodItems.remove(foodItem);
+    return foodItems;
+  }
+}
