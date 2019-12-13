c9ed87ac2f5932d141525e233e02d95d66f44ad1
Breyanna Vass <breyannavass@gmail.com>
2019-12-12 21:24:09 -0500

    adding in bloc and rxdart then bloc files

---
+++ lib/bloc/cartlistBloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:food_delivery/model/fooditem.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CartListBloc extends BlocBase {
+  CartListBloc();
+
+//Stream that receives a number and changes the count;
+  var _listController = BehaviorSubject<List<FoodItem>>.seeded();
+//output
+  Stream<List<FoodItem>> get listStream => _listController.stream;
+//input
+  Sink<List<FoodItem>> get listSink => _listController.sink;
+
+  // Business Logic
+  addToList(FoodItem foodItem) {
+    listSink.add(foodItem);
+  }
+
+//dispose will be called automatically by closing its streams
+  @override
+  void dispose() {
+    _listController.close();
+    super.dispose();
+  }
+}
